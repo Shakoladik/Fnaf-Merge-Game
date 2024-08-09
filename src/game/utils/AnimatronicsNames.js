1c852fa0d4f400ca4dd3b0acd5d3d74398fe1516
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-08-09 10:49:27 +0600

    Add animatronics names enum

---
+++ src/game/utils/AnimatronicsNames.js
@@ -0,0 +1,16 @@
+const AnimatronicsNames = {
+  ENDO: 'endo',
+  BB: 'bb',
+  BONNIE: 'bonnie',
+  TOY_BONNIE: 'toy-bonnie',
+  CHIKA: 'chika',
+  TOY_CHIKA: 'toy-chika',
+  FREDDY: 'freddy',
+  TOY_FREDDY: 'toy-freddy',
+  FOXY: 'foxy',
+  MANGLE: 'mangle',
+  GOLDEN_FREDDY: 'golden-freddy',
+  PUPPET: 'puppet',
+}
+
+export default AnimatronicsNames;
