d3f06872e2528e9a4c69c3069cae0c77a0380bdf
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-08-13 10:09:53 +0600

    Make smoke grow and vanish with transparency

--- src/game/entities/Smoke.js
+++ src/game/entities/Smoke.js
@@ -3,11 +3,27 @@ import Phaser from 'phaser';
 export default class Smoke extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y) {
     super(scene, x, y, 'smoke', 0);
-    
+
+    // Time is in milliseconds
+    this.growthSpeed = 150;
+    this.vanishingSpeed = 300;
+
+    // Set initial state that the smoke will start it's tween from
+    this.initialScale = 0.5
+
+    this.setScale(this.initialScale);
+
     scene.tweens.add({
       targets: this,
-      scale: { from: 1, to: 1.2 },
-      duration: 200,
+      scale: { from: this.initialScale, to: 1.3 },
+      duration: this.growthSpeed,
+    });
+
+    scene.tweens.add({
+      targets: this,
+      delay: this.growthSpeed + 20,
+      alpha: { from: 1, to: 0 },
+      duration: this.vanishingSpeed,
       onComplete: () => {
         this.destroy();
       },
