e7a1db4c5a7daa8a5a2232c769ceb4438e6536f5
Igor Lubenskiy <64972579+EdgeFirewalk@users.noreply.github.com>
2024-08-13 08:44:02 +0600

    Change code identation

--- src/game/entities/Smoke.js
+++ src/game/entities/Smoke.js
@@ -3,6 +3,7 @@ import Phaser from 'phaser';
 export default class Smoke extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y) {
     super(scene, x, y, 'smoke', 0);
+    
     scene.tweens.add({
       targets: this,
       scale: { from: 1, to: 1.2 },
