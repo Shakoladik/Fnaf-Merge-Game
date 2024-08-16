d55999874de72850c00878d1d61d50d81ee01f79
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-08-16 09:39:53 +0600

    Create new class for spawning animatronics

---
+++ src/game/entities/AnimatronicsSpawner.js
@@ -0,0 +1,88 @@
+import Phaser from 'phaser';
+
+import AnimatronicsNames from '../utils/AnimatronicsNames';
+
+import Animatronic from '../entities/Animatronic';
+
+export default class AnimatronicsSpawner {
+  constructor(scene) {
+    this.scene = scene;
+    this.boxHeight = 250;
+    this.boxWidth = 160;
+    this.animatronicsMap = new Map();
+    this.isDrawingSpawnLine = false;
+    this.spawnLineStartPoint = null;
+    this.spawnLineGraphics = this.scene.add.graphics();
+    this.spawnLineLength = 600;
+    this.spawnLineWidth = 2;
+    this.lastSpawnTime = 0;
+    this.spawnCooldown = 320;
+
+    // Add mouse/touch event listeners
+    this.scene.input.on('pointerdown', this.handlePointerDown, this);
+    this.scene.input.on('pointerup', this.handlePointerUp, this);
+    this.scene.input.on('pointermove', this.handlePointerMove, this);
+  }
+
+  handlePointerDown(pointer) {
+    this.isDrawingSpawnLine = true;
+    this.spawnLineStartPoint = { x: pointer.x, y: this.boxHeight };
+    this.updateSpawnLine(pointer);
+  }
+
+  handlePointerMove(pointer) {
+    if (this.isDrawingSpawnLine) {
+      this.updateSpawnLine(pointer);
+    }
+  }
+
+  handlePointerUp(pointer) {
+    if (this.isDrawingSpawnLine) {
+      this.isDrawingSpawnLine = false;
+      this.spawnLineGraphics.clear();
+
+      const currentTime = this.scene.time.now;
+      if (currentTime - this.lastSpawnTime >= this.spawnCooldown) {
+        const centerX = this.scene.game.config.width / 2;
+        let adjustedX = pointer.x;
+        if (Math.abs(pointer.x - centerX) > this.boxWidth) {
+          adjustedX = centerX + Math.sign(pointer.x - centerX) * this.boxWidth;
+        }
+
+        const animatronic = new Animatronic(
+          this.scene,
+          AnimatronicsNames.ENDO,
+          adjustedX,
+          this.boxHeight,
+        );
+
+        this.scene.add.existing(animatronic);
+        this.animatronicsMap.set(animatronic.name, animatronic);
+
+        this.lastSpawnTime = currentTime;
+      }
+    }
+  }
+
+  updateSpawnLine(pointer) {
+    if (this.spawnLineGraphics && this.spawnLineStartPoint) {
+      this.spawnLineGraphics.clear();
+      this.spawnLineGraphics.lineStyle(this.spawnLineWidth, 0xffffff);
+
+      const centerX = this.scene.game.config.width / 2;
+      let adjustedX = pointer.x;
+      if (Math.abs(pointer.x - centerX) > this.boxWidth) {
+        adjustedX = centerX + Math.sign(pointer.x - centerX) * this.boxWidth;
+      }
+
+      const endY = this.boxHeight + this.spawnLineLength;
+      this.spawnLineGraphics.lineBetween(
+        adjustedX,
+        this.boxHeight,
+        adjustedX,
+        endY,
+      );
+      this.spawnLineGraphics.strokePath();
+    }
+  }
+}
